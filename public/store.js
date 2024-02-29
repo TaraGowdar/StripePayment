8776bf4d2794bbda6eb3b962f78782ce4cfb5907
Tara <tarap@ivoyant.com>
2024-02-29 17:24:30 +0530

    init commit

--- public/store.js
+++ public/store.js
@@ -1,77 +1,130 @@
 //if script still loading then display DOmContentLoaded and call ready () else ready() will be called
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
-} else {
-    ready()
-}
+//if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+// } else {
+    // ready();
+// }
 
 function ready() {
     //setting all button to initial values when page loaded
-    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger');
     for (var i = 0; i < removeCartItemButtons.length; i++) {
-        var button = removeCartItemButtons[i]
-        button.addEventListener('click', removeCartItem)
+        var button = removeCartItemButtons[i];
+        button.addEventListener('click', removeCartItem);
     }
 
-    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input');
     for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInputs[i]
-        input.addEventListener('change', quantityChanged)
+        var input = quantityInputs[i];
+        input.addEventListener('change', quantityChanged);
     }
 
-    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    var addToCartButtons = document.getElementsByClassName('shop-item-button');
     for (var i = 0; i < addToCartButtons.length; i++) {
-        var button = addToCartButtons[i]
-        button.addEventListener('click', addToCartClicked)
+        var button = addToCartButtons[i];
+        button.addEventListener('click', addToCartClicked);
     }
 
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked);
 }
 
-function purchaseClicked() {
-    alert('Thank you for your purchase')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    while (cartItems.hasChildNodes()) {// until there is items in the cart loop and remove them from cart
-        cartItems.removeChild(cartItems.firstChild)
+var stripeHandler = StripeCheckout.configure({
+    key: stripePublicKey, //js key
+    locale:'en', //what language to use
+    token: function(token){
+        // how to respond when strip send us back info
+        //after the purchase button is clicked all info is sent to 
+        //stripe and stripe verifies it & call this method
+       var items = [];
+       var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+       var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+       for(var i =0; i < cartRows.length; i++){
+        var cartRow = cartRows [i]; //individual cart row
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+        var quantity = quantityElement.value; //how many coffee cups are added etc details
+        var id = cartRow.dataset.itemId;
+        items.push({
+            id:id,
+            quantity: quantity
+        });
+       }
+       //fetch method is useful to send info to server
+       fetch('/purchase', {
+        method: 'POST',
+        headers : {//below info telling server that  json info is sent and received
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        //we are fetching to the server
+        body: JSON.stringify({
+            stripeTokenId: token.id,
+            items: items
+        })
+       }).then (function(res){
+        return res.json(); //sent from server as json so covert to json
+       }).then(function(data){
+        alert(data.message);
+        var cartItems = document.getElementsByClassName('cart-items')[0];
+        while(cartItems.hasChildNodes()){
+            cartItems.removeChild(cartItems.firstChild);
+        }
+        updateCartTotal();
+       }).catch(function(error){
+        console.error(error);
+       });
     }
-    updateCartTotal()
+    });
+
+function purchaseClicked() {
+    //when purchase button is clicked, stripe is called  and stripe is going to call us back and 
+    //say ok thats valid & then we can call our server and server is 
+    //going to do all checkout info that we need to do
+                   
+    //total value is got from html page
+    var priceElement = document.getElementsByClassName('cart-total-price')[0];
+    var price = parseFloat(priceElement.innerText.replace('$', '')) * 100; //bcoz price in cents is required by stripe
+    stripeHandler.open({
+        amount: price
+    });
 }
 
 function removeCartItem(event) {
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
-    updateCartTotal()
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
 }
 
 function quantityChanged(event) {
-    var input = event.target
+    var input = event.target;
     if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1
+        input.value = 1;
     }
-    updateCartTotal()
+    updateCartTotal();
 }
 
 function addToCartClicked(event) {
-    var button = event.target
-    var shopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src //here pic of item is choosen
-    addItemToCart(title, price, imageSrc)
-    updateCartTotal()
+    var button = event.target;
+    var shopItem = button.parentElement.parentElement;
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src; //here pic of item is choosen
+    var id = shopItem.dataset.itemId;
+    addItemToCart(title, price, imageSrc,id );
+    updateCartTotal();
 }
 
-function addItemToCart(title, price, imageSrc) {
+function addItemToCart(title, price, imageSrc, id) {
     //first cart row creation is required
-    var cartRow = document.createElement('div')
-    cartRow.classList.add('cart-row') //cart row is not yet styled.so it will be added here 
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    var cartRow = document.createElement('div');
+    cartRow.classList.add('cart-row'); //cart row is not yet styled.so it will be added here 
+    cartRow.dataset.itemId = id
+    var cartItems = document.getElementsByClassName('cart-items')[0];
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title');
     for (var i = 0; i < cartItemNames.length; i++) {
         // duplicate of items should not be added--handled in if section below
         if (cartItemNames[i].innerText == title) {
-            alert('This item is already added to the cart')
-            return
+            alert('This item is already added to the cart');
+            return;
         }
     }
     //new cart row is created here--so copy from store.html directly  as string in back ticks
@@ -82,33 +135,33 @@ function addItemToCart(title, price, imageSrc) {
         </div>
         <span class="cart-price cart-column">${price}</span>
         <div class="cart-quantity cart-column">
-            <input class="cart-quantity-input" type="number" value="1">
+            <input class="cart-quantity-input" type="number" value="1" autocomplete="off">
             <button class="btn btn-danger" type="button">REMOVE</button>
         </div>`
         // innerHtml is used here because HTML tags are used inside of the text
-    cartRow.innerHTML = cartRowContents
-    cartItems.append(cartRow)
-    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+    cartRow.innerHTML = cartRowContents;
+    cartItems.append(cartRow);
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem);
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged);
 }
 
 function updateCartTotal() {
     // cart-items is an array so need to get the first item is enough
-    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0];
     //cartRows added one by one in for
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    var total = 0
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+    var total = 0;
     for (var i = 0; i < cartRows.length; i++) {
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
         //$9.99 is changed to 9.99 to calculate total and parseFloat will convert 9.99 to float instad of a string
-        var price = parseFloat(priceElement.innerText.replace('$', ''))
-        var quantity = quantityElement.value
-        total = total + (price * quantity)
+        var price = parseFloat(priceElement.innerText.replace('$', ''));
+        var quantity = quantityElement.value;
+        total = total + (price * quantity);
     }
     //round the total to the nearest value
-    total = Math.round(total * 100) / 100
+    total = Math.round(total * 100) / 100;
     //finally after total 4$ will be added
-    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total;
 }
--- server.js
+++ server.js
@@ -1,12 +1,81 @@
+if(process.env.NODE_ENV !== 'production')
+{
+    dotenv.config();
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey= process.env.STRIPE_PUBLIC_KEY;
+
 import express from "express";
-import stripe from "stripe";
+import dotenv from "dotenv";
+import fs from "fs"; //include to read file
+//how to pass secret key to stripe to activate API
+import stripePackage from "stripe";
+
+const stripe =new stripePackage(stripeSecretKey);
 
 const app = express(); //create our app
 
 //set view engine that v r going to be using our application this will allow us to embed server-side code inside of out front-end HTML pages 
 //front end will be using ejs to render its views
 app.set('view engine', 'ejs');
+//
+app.use(express.json());
 //where our static files be stored is indicated in mentioned in the code below
 app.use(express.static('public'));
 
-app.listen(3000);
+
+app.get('/store', function(req,res)  {
+    fs.readFile('items.json', function(error, data){
+        if(error)
+        { 
+            res.status(500).end();//when error end it
+        }
+        else{ 
+            //we have to render store.html file but have to access values from the express page so store.ejs is used
+            res.render('store.ejs', {
+                //sending the public key to store.ejs as JS variable. so easy to access in Client side JS 
+                stripePublicKey: stripePublicKey,
+                //pass various items for it to display
+                items: JSON.parse(data)
+            });
+        }
+    });
+});
+
+app.post('/purchase', function(req,res){
+    fs.readFile('items.json', function(error, data){
+        if(error){ res.status(500).end(); }
+        else{
+            //all the data from json is here in the variable
+            const itemsJson = JSON.parse(data);
+            //merge both merch & music in to single array
+            const itemsArray = itemsJson.music.concat(itemsJson.merch);
+            let total = 0;
+            //body has json content we need to access it so include app.use statement up in the begining with json
+            req.body.items.forEach(function(item) {
+                const itemJson = itemsArray.find(function(i){
+                    return i.id == item.id;
+                });
+                total = total + itemJson.price * item.quantity;
+            });
+
+            stripe.charges.create({
+                amount: total,
+                source: req.body.stripeTokenId,
+                currency: 'usd'
+            }).then(function() { //success of the charging
+                console.log('Charge Successful');
+                res.json({message: 'Successfully purchased items'});
+            }).catch(function(){//failure of charging
+                console.log('Charge Fail');
+                res.status(500).end();
+            });
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000; // Use the provided port or default to 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
